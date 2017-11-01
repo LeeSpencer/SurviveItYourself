105a23e50365952de2ab1058f89d5d3b2965450d
surviveityourself <lees18@charlottenet.org>
2017-11-01 15:53:16 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@ $mail->isSMTP();                                      			 // Set mailer to use S
 $mail->Host = 'smtp.gmail.com';  								 // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               			 // Enable SMTP authentication
 $mail->Username = 'lees18@charlottenet.org';                 	 // SMTP username
-$mail->Password = '2Have1pad';                           		 // SMTP password
+$mail->Password = '********';                           		 // SMTP password
 $mail->SMTPSecure = 'tls';                            			 // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    			 // TCP port to connect to
 
