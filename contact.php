c8e8cc124ce9e132ea8a775c671a933dc6ea528b
surviveityourself <lees18@charlottenet.org>
2017-11-01 15:53:42 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      			 // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  								 // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               			 // Enable SMTP authentication
-$mail->Username = 'lees18@charlottenet.org';                 	 // SMTP username
+$mail->Username = 'johndoe@email.com';                       	 // SMTP username
 $mail->Password = '********';                           		 // SMTP password
 $mail->SMTPSecure = 'tls';                            			 // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    			 // TCP port to connect to
